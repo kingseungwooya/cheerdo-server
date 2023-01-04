871f18b889a39af8b536d92984bf97198145dd04
kingseungwooya <ksw_0505@nate.com>
2023-01-05 00:06:07 +0900

    initialize: jump into cheer-do server

---
+++ src/main/java/com/example/cheerdo/CheerdoApplication.java
@@ -0,0 +1,13 @@
+package com.example.cheerdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheerdoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheerdoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cheerdo/CheerdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cheerdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheerdoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
