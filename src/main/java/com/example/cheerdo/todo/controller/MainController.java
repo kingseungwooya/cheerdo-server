cf4f51f1551832a18ca6b45351c6f7be7d4ead70
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-01-11 13:01:08 +0900

    refact(main): use faker to random generate

--- src/main/java/com/example/cheerdo/todo/controller/MainController.java
+++ src/main/java/com/example/cheerdo/todo/controller/MainController.java
@@ -1,19 +1,27 @@
 package com.example.cheerdo.todo.controller;
 
+import com.example.cheerdo.todo.dto.request.GetTodoRequestDto;
 import com.example.cheerdo.todo.dto.request.WriteTodoRequestDto;
 import com.example.cheerdo.todo.dto.response.TodoResponseDto;
+import com.github.javafaker.Faker;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/main")
 public class MainController {
 
+    private final Faker faker = new Faker();
+
     @PostMapping("/todo")
     public ResponseEntity<Long> writeTodo(@RequestBody WriteTodoRequestDto todoDto) {
-        Long todoId = 1L;
+        Long todoId = faker.number().randomNumber();
         return new ResponseEntity<>(todoId, HttpStatus.OK);
     }
-    
+
+   
+
 }
