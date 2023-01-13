a770d230fbac67b86d6fee227d457f4f6c562cdf
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-01-13 20:47:24 +0900

    fix: add cors configuration

--- src/main/java/com/example/cheerdo/CheerdoApplication.java
+++ src/main/java/com/example/cheerdo/CheerdoApplication.java
@@ -3,12 +3,25 @@ package com.example.cheerdo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class CheerdoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CheerdoApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 
 }
