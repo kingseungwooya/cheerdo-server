28f34c09f1de1cf00efa3592ffb0cd1628e43987
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-01-20 21:30:26 +0900

    feat(login): apply service inreface

--- src/main/java/com/example/cheerdo/login/service/MemberService.java
+++ src/main/java/com/example/cheerdo/login/service/MemberService.java
@@ -1,11 +1,20 @@
 package com.example.cheerdo.login.service;
 
+import com.example.cheerdo.entity.Role;
 import com.example.cheerdo.login.dto.request.JoinRequestDto;
-import com.example.cheerdo.login.dto.response.JoinResponseDto;
+import com.example.cheerdo.login.dto.response.MemberInfoResponseDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public interface MemberService {
 
-    JoinResponseDto join(JoinRequestDto joinRequestDto);
+    void join(JoinRequestDto joinRequestDto);
+
+    Role saveRole(Role role);
+
+    void addToRoleToUser(String userid, String roleName);
+
+    MemberInfoResponseDto getInfoById(String memberId);
+    
+    boolean checkUsernameDuplication(String memberId);
 }
