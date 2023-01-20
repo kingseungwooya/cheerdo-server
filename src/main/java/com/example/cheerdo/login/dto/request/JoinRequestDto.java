2e4f62e587029ecf1369563217d748d2e4b7cbb7
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-01-20 21:29:08 +0900

    refactor(login): create default value of member

--- src/main/java/com/example/cheerdo/login/dto/request/JoinRequestDto.java
+++ src/main/java/com/example/cheerdo/login/dto/request/JoinRequestDto.java
@@ -1,10 +1,22 @@
 package com.example.cheerdo.login.dto.request;
 
 
+import com.example.cheerdo.entity.Member;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Id;
 import javax.validation.constraints.*;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class JoinRequestDto {
+
+    private static final int DEFAULT_COIN_COUNT = 10;
+    private static final int DEFAULT_HABIT_PROGRESS = 0;
 
     @Size(min = 8, max = 12, message = "아이디는 8~12 글자 사이로 만들어주세요. ")
     @NotEmpty(message = "아이디는 필수 입력 값입니다.")
@@ -24,11 +36,24 @@ public class JoinRequestDto {
     @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "이름은 특수문자를 제외한 2~10자리여야 합니다.")
     private String name;
 
+    private String bio;
+
     public boolean isSamePassword() {
         if (this.password.equals(this.passwordConfirm)) {
             return true;
         }
         return false;
+    }
+
+    public Member dtoToMember() {
+        return Member.builder()
+                .id(memberId)
+                .password(password)
+                .bio(bio)
+                .name(name)
+                .coinCount(DEFAULT_COIN_COUNT)
+                .habitProgress(DEFAULT_HABIT_PROGRESS)
+                .build();
     }
 
 }
