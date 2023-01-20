2d56435aa5c235e922d0c75b1ddab7d21b5dbf53
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-01-20 21:25:20 +0900

    fix(login): fix args of member repo

--- src/main/java/com/example/cheerdo/login/repository/MemberRepository.java
+++ src/main/java/com/example/cheerdo/login/repository/MemberRepository.java
@@ -1,8 +1,9 @@
 package com.example.cheerdo.login.repository;
 
+import com.example.cheerdo.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MemberRepository extends JpaRepository<MemberRepository, String> {
+public interface MemberRepository extends JpaRepository<Member, String> {
 }
