f952bd368c81d9649ad6854b52ae8b45eb05f481
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-01-24 19:41:39 +0900

    feat(post): refactoring controller

--- src/main/java/com/example/cheerdo/post/controller/PostController.java
+++ src/main/java/com/example/cheerdo/post/controller/PostController.java
@@ -1,60 +1,58 @@
 package com.example.cheerdo.post.controller;
 
+import com.example.cheerdo.post.dto.request.LetterRequestDto;
+import com.example.cheerdo.post.dto.request.PostRequestDto;
 import com.example.cheerdo.post.dto.response.PostResponseDto;
 import com.example.cheerdo.post.dto.response.PostStatusResponse;
-import com.example.cheerdo.post.dto.response.letterResponseDto;
+import com.example.cheerdo.post.dto.response.LetterResponseDto;
+import com.example.cheerdo.post.service.PostService;
 import com.github.javafaker.Faker;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import io.swagger.annotations.Example;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Locale;
 
 @RestController
 @RequestMapping("api/post")
-
+@RequiredArgsConstructor
 public class PostController {
-    private final Faker faker = new Faker(Locale.KOREAN);
     private final Logger logger = LoggerFactory.getLogger(PostController.class);
+    private final PostService postService;
 
-    @GetMapping("/new-post")
+    @GetMapping("/posts")
     @ApiOperation(value = "PostBox를 눌렀을때 나오는 읽지 않은 편지들을 반환"
             , notes = "사용자의 ID를 통해 조회한다. ")
     @ApiResponses( {
             @ApiResponse( code = 200, message = "status ok")
     })
-    public ResponseEntity<?> getMyNewPost(@RequestParam Long userId) {
-        logger.info("request userId is -> {}", userId);
-        List<PostResponseDto> responseDtos = List.of(
-                new PostResponseDto("senderID1", faker.leagueOfLegends().champion(), faker.number().randomNumber()),
-                new PostResponseDto("senderID2", faker.leagueOfLegends().champion(), faker.number().randomNumber()),
-                new PostResponseDto("senderID3", faker.leagueOfLegends().champion(), faker.number().randomNumber())
-        );
-        return new ResponseEntity<>(responseDtos, HttpStatus.OK);
+    public ResponseEntity<?> getMyNewPost(@ModelAttribute PostRequestDto postRequestDto) {
+        logger.info("request is -> {}", postRequestDto);
+        try {
+            List<PostResponseDto> postResponseDtos = postService.getMyPosts(postRequestDto);
+            return new ResponseEntity<>(postResponseDtos, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 
     @GetMapping("/letter")
     public ResponseEntity<?> readLetter(@RequestParam Long letterId) {
-        // 여기서 처리
-        logger.info("request letterId is -> {}", letterId);
-        if (letterId == 0) {
-            // 406
-            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
-        }
-        letterResponseDto responseDto =
-                new letterResponseDto(faker.number().randomNumber(), "senderId1", faker.leagueOfLegends().champion(),
-                        faker.book().title(), faker.lorem().sentence(30));
-        return new ResponseEntity<>(responseDto, HttpStatus.OK);
+        return new ResponseEntity<>(postService.readPost(letterId), HttpStatus.OK);
+    }
+
+    @PostMapping("/letter")
+    public ResponseEntity<?> writeLetter(@RequestBody LetterRequestDto letterRequestDto) {
+        postService.writeLetter(letterRequestDto);
+        return new ResponseEntity<>("편지를 성공적으로 보냈습니다", HttpStatus.OK);
     }
 
     @GetMapping("/status")
