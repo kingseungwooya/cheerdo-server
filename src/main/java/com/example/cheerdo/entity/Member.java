b32e850dc1472ecfb36d8674c880bcc3b059a47a
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-01-25 14:47:15 +0900

    feat(post): add setter to modifying

--- src/main/java/com/example/cheerdo/entity/Member.java
+++ src/main/java/com/example/cheerdo/entity/Member.java
@@ -3,6 +3,7 @@ package com.example.cheerdo.entity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -11,6 +12,7 @@ import javax.persistence.Lob;
 
 @Entity
 @Getter
+@Setter
 @NoArgsConstructor
 public class Member {
     @Id
