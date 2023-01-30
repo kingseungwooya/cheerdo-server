6d9807469db47ce10e6eda4cb272d88ccbf67f52
bodol4748 <55184097+bodol4748@users.noreply.github.com>
2023-01-30 22:12:41 +0900

    chore(friends): add logger and rename variable and exception
    In FriendRelationServicelmpl.java
    Rename Exception
    Line 42 Exception "친구가 없습니다" -> "member has no friends"
    
    Rename variable
    Line 46 friend_member -> frinedMember
    
    Add Faker for friendRelation Id

--- src/main/java/com/example/cheerdo/friendList/service/FriendRelationServiceImpl.java
+++ src/main/java/com/example/cheerdo/friendList/service/FriendRelationServiceImpl.java
@@ -7,6 +7,8 @@ import com.example.cheerdo.friendList.dto.request.PutRequestDto;
 import com.example.cheerdo.friendList.dto.response.LoadFriendResponseDto;
 import com.example.cheerdo.friendList.repository.FriendRelationRepository;
 import com.example.cheerdo.friendList.repository.MemberRepository;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.github.javafaker.Faker;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,33 +23,40 @@ import java.util.Optional;
 @AllArgsConstructor
 @Transactional
 public class FriendRelationServiceImpl implements FriendRelationService {
+    private final Logger logger = LoggerFactory.getLogger(FriendRelationServiceImpl.class);
     private final MemberRepository memberRepository;
     private final FriendRelationRepository friendRelationRepository;
+    private final Faker faker = new Faker();
     @Override
     public List<?> getMyFriendList(String userId) throws Exception {
         // 반환값으로 relation id member id name을 가지는 LoadFriendRelationDto의 List가 반환된다.
 
         Optional<Member> member = memberRepository.findById(userId);
+        logger.info("member is -> {}", member.get().getName());
+
         Optional<List<FriendRelation>> friendRelations = friendRelationRepository.findAllByMember(member);
+        logger.info("friendRelations size is -> {}", friendRelations.get().size());
         List<LoadFriendResponseDto> list = new ArrayList<>();
 
-        if (friendRelations.isEmpty()) {
-            throw new Exception("친구가 없습니다");
+        if (friendRelations.get().isEmpty()) {
+            throw new Exception("member has no friends");
         }
 
         for (FriendRelation relation : friendRelations.get()) {
-            Optional<Member> frined_member = memberRepository.findById(relation.getFriendId());
+            Optional<Member> friendMember = memberRepository.findById(relation.getFriendId());
             list.add(LoadFriendResponseDto.builder()
-                    .name(frined_member.get().getName())
+                    .name(friendMember.get().getName())
                     .relationId(relation.getId())
                     .memberId(relation.getFriendId())
                     .build());
+            logger.info("build complete");
         }
+        logger.info("List complete");
         return list;
     }
 
     public void putRequest(PutRequestDto putRequestDto) throws Exception {
-        Long id = 5L;
+        Long id = faker.number().randomNumber();
         friendRelationRepository.save(putRequestDto.dtoToFriendRelationEntity(id, memberRepository.findById(putRequestDto.getUserId())));
     }
 
