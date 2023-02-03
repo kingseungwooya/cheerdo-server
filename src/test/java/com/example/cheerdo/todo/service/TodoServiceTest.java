d68dacb84f12adda86a80490abd5d89c09eb63aa
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-02-03 15:50:12 +0900

    test(main): test for update and delete

--- src/test/java/com/example/cheerdo/todo/service/TodoServiceTest.java
+++ src/test/java/com/example/cheerdo/todo/service/TodoServiceTest.java
@@ -5,6 +5,7 @@ import com.example.cheerdo.entity.Todo;
 import com.example.cheerdo.repository.MemberRepository;
 import com.example.cheerdo.repository.TodoRepository;
 import com.example.cheerdo.todo.dto.request.GetTodoRequestDto;
+import com.example.cheerdo.todo.dto.request.ModifyTodoRequestDto;
 import com.example.cheerdo.todo.dto.request.WriteTodoRequestDto;
 import com.example.cheerdo.todo.dto.response.TodoResponseDto;
 import com.example.cheerdo.todo.enums.Type;
@@ -22,6 +23,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import static org.hamcrest.Matchers.*;
@@ -120,29 +122,71 @@ class TodoServiceTest {
     }
 
     @Test
+    @DisplayName("수정이 잘 이루어지는지 확인한다. ")
     void modifyTodo() {
         // given
+        WriteTodoRequestDto writeTodoRequestDto =
+                new WriteTodoRequestDto(member.getId(), Type.TODO.name(), "오늘의 할일!~");
+        Long todoId = todoService.writeTodo(writeTodoRequestDto);
 
         // when
+        todoService.modifyTodo(new ModifyTodoRequestDto(todoId, "수정된 할일"));
 
         // then
+        Todo todo = todoRepository.findById(todoId).get();
+        assertThat(writeTodoRequestDto.getTodo(), not(todo.getContent()));
+        assertThat(todoId, is(todo.getId()));
     }
 
     @Test
+    @DisplayName("Todo를 삭제할 수 있다. ")
     void deleteTodo() {
         // given
+        WriteTodoRequestDto writeTodoRequestDto =
+                new WriteTodoRequestDto(member.getId(), Type.TODO.name(), "곧 지워질 할일!~");
+        Long todoId = todoService.writeTodo(writeTodoRequestDto);
 
         // when
+        todoService.deleteTodo(todoId);
 
         // then
+        Optional<Todo> todo = todoRepository.findById(todoId);
+        assertThat(todo.isEmpty(), is(true));
+        assertThat(todo.isPresent(), is(false));
     }
 
     @Test
+    @DisplayName("성공상태에서 실패로, 실패에서 성공으로")
     void success() {
         // given
+        Todo todo = Todo.builder()
+                .member(member)
+                .isSuccess(false)
+                .date(testDate)
+                .type(Type.TODO)
+                .content("테스트용")
+                .build();
+        Long todoId = todoRepository.save(todo).getId();
+
+        Todo successTodo = Todo.builder()
+                .member(member)
+                .isSuccess(true)
+                .date(testDate)
+                .type(Type.TODO)
+                .content("테스트용")
+                .build();
+        Long successTodoId = todoRepository.save(successTodo).getId();
 
         // when
+        todoService.success(todoId);
+        todoService.success(successTodoId);
 
         // then
+        // 취소를 성공으로
+        Optional<Todo> testTodo = todoRepository.findById(todoId);
+        assertThat(testTodo.get().isSuccess(), is(true));
+        // 성공을 취소로
+        Optional<Todo> testTodo2 = todoRepository.findById(successTodoId);
+        assertThat(testTodo2.get().isSuccess(), is(false));
     }
 }
