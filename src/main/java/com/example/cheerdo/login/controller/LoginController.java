db90743822661d7785c688fe4d1b325d436d5a7e
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-02-05 20:57:25 +0900

    feat(login): add dto and some of api

--- src/main/java/com/example/cheerdo/login/controller/LoginController.java
+++ src/main/java/com/example/cheerdo/login/controller/LoginController.java
@@ -1,8 +1,10 @@
 package com.example.cheerdo.login.controller;
 
+import com.example.cheerdo.login.dto.response.MemberInfoResponseDto;
 import com.example.cheerdo.login.dto.response.error.ErrorResponseDto;
 import com.example.cheerdo.login.dto.request.JoinRequestDto;
 import com.example.cheerdo.login.service.MemberService;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,7 +28,7 @@ public class LoginController {
             ErrorResponseDto apiError = new ErrorResponseDto(HttpStatus.BAD_REQUEST, bindingResult.getFieldError().getDefaultMessage());
             return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
         }
-        if (request.isSamePassword()) {
+        if (!request.isSamePassword()) {
             ErrorResponseDto apiError = new ErrorResponseDto(HttpStatus.BAD_REQUEST
                     , "입력된 비밀번호가 동일하지 않습니다.");
             return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
@@ -36,10 +38,33 @@ public class LoginController {
                     , "중복된 id가 존재합니다. ");
             return new ResponseEntity<>(apiError, HttpStatus.BAD_REQUEST);
         }
-
         memberService.join(request);
+        memberService.addToRoleToUser(request.getMemberId(), "ROLL_USER");
         return new ResponseEntity<>(HttpStatus.OK);
     }
+    @PostMapping
+    public ResponseEntity<Boolean> checkMemberIdDuplicated(@RequestParam("memberId") String memberId) {
+        return new ResponseEntity<>(memberService.checkUsernameDuplication(memberId), HttpStatus.OK);
+    }
 
+    @GetMapping("/user/info")
+    public ResponseEntity<MemberInfoResponseDto> getMemberInfo(@RequestParam String memberId) {
+        return ResponseEntity.ok().body(memberService.getInfoById(memberId));
+    }
+
+    @PostMapping("/role/addtouser")
+    public ResponseEntity<?>addRoleToMember(@RequestBody RoleToMemberForm form){
+        memberService.addToRoleToUser(form.getMemberId(),form.getRoleName());
+        return ResponseEntity.ok().build(); // 코드 200 반환
+    }
+
+
+}
+// Dto
+@Data
+class RoleToMemberForm{
+
+    private String memberId;
+    private String roleName;
 
 }
