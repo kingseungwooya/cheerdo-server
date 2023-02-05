277c94acf9caf7caca101b2a33ee66d0a183b10b
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-02-05 20:59:04 +0900

    fix(login): delete duplicated repo

--- src/main/java/com/example/cheerdo/login/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.cheerdo.login.repository;
-
-import com.example.cheerdo.entity.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MemberRepository extends JpaRepository<Member, String> {
-}
