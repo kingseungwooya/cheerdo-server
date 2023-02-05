e1ebfd4de50e2a38b7b06ce5326bd4ba7575229d
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-02-05 20:59:30 +0900

    feat(login): add features member service

--- src/main/java/com/example/cheerdo/login/service/MemberServiceImpl.java
+++ src/main/java/com/example/cheerdo/login/service/MemberServiceImpl.java
@@ -5,23 +5,24 @@ import com.example.cheerdo.entity.Role;
 import com.example.cheerdo.login.config.CustomUser;
 import com.example.cheerdo.login.dto.request.JoinRequestDto;
 import com.example.cheerdo.login.dto.response.MemberInfoResponseDto;
-import com.example.cheerdo.login.repository.MemberRepository;
 import com.example.cheerdo.login.repository.RoleRepository;
+import com.example.cheerdo.post.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.Collection;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MemberServiceImpl implements MemberService, UserDetailsService {
     private final MemberRepository memberRepository;
     private final RoleRepository roleRepository;
@@ -41,43 +42,48 @@ public class MemberServiceImpl implements MemberService, UserDetailsService {
     }
 
     @Override
-    public void addToRoleToUser(String userid, String roleName) {
-
+    public void addToRoleToUser(String userId, String roleName) {
+        logger.info("회원:{} 에게 권한:{} 부여 완료.", userId, roleName);
+        Member member = memberRepository.findById(userId).get();
+        Role role = roleRepository.findByName(roleName);
+        logger.info("rolename -> {} 하고 role -> {} 적용되었는지 확인", roleName, role);
+        member.getRoles().add(role);
+        logger.info("role 추가되었는지 확인{}", member);
     }
 
     @Override
     public MemberInfoResponseDto getInfoById(String memberId) {
-        return null;
+        return memberRepository.findById(memberId).get().to();
     }
 
     @Override
     public boolean checkUsernameDuplication(String memberId) {
-        return false;
+        return memberRepository.existsById(memberId);
     }
 
+    // 로그인 요청시 일어나는 service
     @Override
-    public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
-        Member member = memberRepository.findById(memberId).get();
-        if (member == null) {
-            logger.error("해당 id의 회원은 DB에 존재하지 않음");
-            throw new UsernameNotFoundException("해당 id의 회원은 DB에 존재하지 않음");
-        } else {
-            logger.info("멤버가 존재함: {} ", member);
-        }
+    public CustomUser loadUserByUsername(String memberId) throws UsernameNotFoundException {
+        Member member = memberRepository
+                .findById(memberId)
+                .orElseThrow(() -> new UsernameNotFoundException("해당 id의 회원은 DB에 존재하지 않음"));
+        logger.info("멤버가 존재함: {} ", member.getId());
         Collection<SimpleGrantedAuthority> authorities = new ArrayList<>();
         member.getRoles().forEach(role -> {
-            authorities.add(new SimpleGrantedAuthority(role.getMemberId()));
+            authorities.add(new SimpleGrantedAuthority(role.getName()));
         });
         // 편지 갯수를 반환하기 위해선 post repo를 통해
         // recevier_id가 본인이고 isOpen이 false인 값을 찾아 개수를 센다
         int letterCount = 0; // 추후 구현
         // spring secirity 사용자를 return
-        return CustomUser.builder()
-                .username(member.getId())
-                .password(member.getPassword())
-                .authorities(authorities)
+        return new CustomUser(member.getId(), member.getPassword(), authorities, member.getCoinCount(), letterCount);
+        /* CustomUser.builder()
+                .clone(User.builder()
+                        .username(member.getId())
+                        .password(member.getPassword())
+                        .authorities(authorities))
                 .coinCount(member.getCoinCount())
                 .newLetterCount(letterCount)
-                .build();
+                .build();*/
     }
 }
