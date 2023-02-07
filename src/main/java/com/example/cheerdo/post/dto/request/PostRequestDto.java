226947d78b4f509662621a9cd050c9972cdc1c68
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-02-07 20:18:16 +0900

    feat(post): add more request parameters

--- src/main/java/com/example/cheerdo/post/dto/request/PostRequestDto.java
+++ src/main/java/com/example/cheerdo/post/dto/request/PostRequestDto.java
@@ -1,11 +1,17 @@
 package com.example.cheerdo.post.dto.request;
 
+import com.example.cheerdo.common.enums.SortType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.time.LocalDate;
 
 @Getter
 @AllArgsConstructor
 public class PostRequestDto {
     private boolean open;
     private String memberId;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private SortType sortType;
 }
