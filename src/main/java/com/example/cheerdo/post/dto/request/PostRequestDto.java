be2f6e20edd3d8ed29a13f457460f2d52d9e2377
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-02-07 20:27:01 +0900

    fix(post): add date time format annotation

--- src/main/java/com/example/cheerdo/post/dto/request/PostRequestDto.java
+++ src/main/java/com/example/cheerdo/post/dto/request/PostRequestDto.java
@@ -3,6 +3,7 @@ package com.example.cheerdo.post.dto.request;
 import com.example.cheerdo.common.enums.SortType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -11,7 +12,9 @@ import java.time.LocalDate;
 public class PostRequestDto {
     private boolean open;
     private String memberId;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
     private SortType sortType;
 }
