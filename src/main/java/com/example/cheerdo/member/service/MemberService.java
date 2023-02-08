f3bbe00cbebe5e966e0d769e47601e3a7fb9448a
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-02-08 17:25:34 +0900

    fix(member): fix dto name

--- src/main/java/com/example/cheerdo/member/service/MemberService.java
+++ src/main/java/com/example/cheerdo/member/service/MemberService.java
@@ -1,13 +1,12 @@
 package com.example.cheerdo.member.service;
 
-import com.example.cheerdo.member.dto.request.UpdateProfileDto;
+import com.example.cheerdo.member.dto.request.UpdateProfileRequestDto;
 import com.example.cheerdo.member.dto.response.MemberInfoResponseDto;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
 public interface MemberService {
-    String updateMyInfo(UpdateProfileDto updateProfileDto) throws IOException;
+    String updateMyInfo(UpdateProfileRequestDto updateProfileRequestDto) throws IOException;
 
     MemberInfoResponseDto getInfoById(String memberId);
 
