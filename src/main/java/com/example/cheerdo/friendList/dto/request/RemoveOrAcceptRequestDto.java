aa350ab175c5eae2b4022f56c9a51c975a24ab6b
bodol4748 <55184097+bodol4748@users.noreply.github.com>
2023-02-13 02:33:06 +0900

    feat(friends): create new dto for removeoracceptrequest
    It has builder to create friendRelation
    - relationId : FriendRelation's Id
    - accept : if true, it means user wants to accpet this request,
    if false it means user wants to remove this request

---
+++ src/main/java/com/example/cheerdo/friendList/dto/request/RemoveOrAcceptRequestDto.java
@@ -0,0 +1,24 @@
+package com.example.cheerdo.friendList.dto.request;
+
+import com.example.cheerdo.entity.FriendRelation;
+import com.example.cheerdo.entity.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Optional;
+
+@Getter
+@NoArgsConstructor
+public class RemoveOrAcceptRequestDto {
+    private Long relationId;
+    private boolean accept;
+
+    public FriendRelation dtoToFriendRelationEntity(Optional<Member> member, String friendId) {
+        return FriendRelation.builder()
+                .friendId(friendId)
+                .member(member.get())
+                .isFriend(true)
+                .hasMessage(false)
+                .build();
+    }
+}
