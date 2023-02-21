66c1a13f22fec621caa77e221d64ee55721bdc96
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-02-21 17:12:55 +0900

    fix: update cors configure

--- src/main/java/com/example/cheerdo/login/config/SpringSecurity.java
+++ src/main/java/com/example/cheerdo/login/config/SpringSecurity.java
@@ -17,6 +17,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -29,7 +34,7 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final TokenProvider jwtUtil;
-
+    
     @Override
     public void configure(WebSecurity web) {
         web.ignoring().antMatchers("/api/token/refresh", "/api/join");
@@ -50,8 +55,6 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
 
 
         http
-                .cors()
-                .disable()
                 .csrf()
                 .disable()
                 .sessionManagement()
