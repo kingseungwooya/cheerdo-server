a44879755ed753cdf52da0ff01d2d5f3d33afc13
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-03-02 21:03:27 +0900

    feat(main): create calender entity

---
+++ src/main/java/com/example/cheerdo/entity/Calender.java
@@ -0,0 +1,63 @@
+package com.example.cheerdo.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+
+import java.time.LocalDate;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Calender {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "calender_id")
+    private Long calenderId;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate date;
+
+    @Column(name = "success_rate")
+    private double successRate;
+
+    @OneToMany(mappedBy = "calender")
+    private List<Todo> todos = new ArrayList<>();
+
+    @Builder
+    public Calender(Long calenderId, Member member, LocalDate date) {
+        this.calenderId = calenderId;
+        this.member = member;
+        this.date = date;
+        this.successRate = 0;
+    }
+
+    public void updateRate() {
+        long totalSize = todos.size();
+        long successCount = todos.stream()
+                .filter( todo -> todo.isSuccess())
+                .count();
+        this.successRate = (successCount / totalSize) * 100;
+
+    }
+
+    public void addTodo(Todo todo) {
+        todos.add(todo);
+    }
+
+
+}
