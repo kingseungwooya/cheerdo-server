d305bbbe23a943bc4b0408dcfa0e27388e24dc80
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-03-03 21:11:55 +0900

    feat(habit): create habit request dto

---
+++ src/main/java/com/example/cheerdo/todo/dto/request/WriteHabitRequestDto.java
@@ -0,0 +1,27 @@
+package com.example.cheerdo.todo.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class WriteHabitRequestDto {
+    private String memberId;
+
+    private String type;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate startDate;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+
+    private String content;
+
+
+}
