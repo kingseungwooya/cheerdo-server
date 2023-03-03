423b7a07787c02bce9356ee353fc72808cc16bef
bodol4748 <55184097+bodol4748@users.noreply.github.com>
2023-03-03 14:27:44 +0900

    feat(friends): create new dto for getreceivedpostrequest
    create GetReceivedPostRequestDto
    it has sendDateTime, friendId, friendName

---
+++ src/main/java/com/example/cheerdo/friends/dto/response/GetReceivedPostRequestResponseDto.java
@@ -0,0 +1,16 @@
+package com.example.cheerdo.friends.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Builder
+@Getter
+@AllArgsConstructor
+public class GetReceivedPostRequestResponseDto {
+    private LocalDateTime sendDateTime;
+    private String friendId;
+    private String friendName;
+}
