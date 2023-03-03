635a8414771a90b36df7693c20229b8c0d0536fa
김승우 <86772586+kingseungwooya@users.noreply.github.com>
2023-03-03 21:10:33 +0900

    feat(habit): create habit entity

---
+++ src/main/java/com/example/cheerdo/entity/Habit.java
@@ -0,0 +1,40 @@
+package com.example.cheerdo.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+
+import java.time.LocalDate;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Habit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "habit_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Lob
+    private String content;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDate endDate;
+
+    @Builder
+    public Habit(Long id, Member member, String content, LocalDate endDate) {
+        this.id = id;
+        this.member = member;
+        this.content = content;
+        this.endDate = endDate;
+    }
+}
